a35d378327b721b14af36fb6d66eed87e0aa3276
danielalexis <danielalexis.o.pereira@gmail.com>
2023-11-14 17:30:32 +0000

    add BearerAuthenticate middleware > In the future consider OAuth 2.0

--- middleware/main.go
+++ middleware/main.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
@@ -18,4 +21,29 @@ func DontCache() gin.HandlerFunc {
 		ctx.Writer.Header().Set("Cache-Control", "no-store")
 		ctx.Next()
 	}
+}
+
+func BearerAuthenticate() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		// Extract the token from the Authorization header
+		// The header format is "Bearer <token>"
+		token := strings.TrimPrefix(authHeader, "Bearer ")
+
+		// Validate the token
+		// In this example, we're just checking if it matches a predefined token
+		if token != "expected_token" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			return
+		}
+
+		// If the token is valid, continue with the request
+		ctx.Next()
+	}
+
 }
---
+++ models/error.go
@@ -0,0 +1,6 @@
+package main
+
+type ErrorMessage struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
