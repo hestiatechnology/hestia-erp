d611d6b8ebba843b47da8bbdfe84710a28bb4011
danielalexis <danielalexis.o.pereira@gmail.com>
2023-12-24 20:11:03 +0000

    Update logger for production

--- logger/main.go
+++ logger/main.go
@@ -14,11 +14,16 @@ var (
 )
 
 func initializeLogger() {
-	file := os.Stdout
-	// file, err := os.OpenFile("hestia.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	// if err != nil {
-	// 	log.Fatal("Unable to open log file, error: ", err)
-	// }
+	var file *os.File
+	if os.Getenv("HESTIA_ENV") == "production" {
+		var err error
+		file, err = os.OpenFile("hestia.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatal("Unable to open log file, error: ", err)
+		}
+	} else {
+		file = os.Stdout
+	}
 
 	Info = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 	Warning = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
