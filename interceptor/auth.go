826e87fc8f9fcb417f31716b2c46a4219dfde688
danielalexis <danielalexis.o.pereira@gmail.com>
2024-02-19 15:53:18 +0000

    Add AuthInterceptor to gRPC server

---
+++ interceptor/auth.go
@@ -0,0 +1,51 @@
+package interceptor
+
+import (
+	"context"
+	"hestia/api/pb"
+	"hestia/api/utils/user"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+// AuthInterceptor is a gRPC interceptor that checks for a valid token in the metadata
+func AuthInterceptor(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+	// Methods to not check
+	methods := []string{
+		pb.IdentityManagementService_Alive_FullMethodName,
+		pb.IdentityManagementService_Login_FullMethodName,
+		pb.IdentityManagementService_Logout_FullMethodName,
+		pb.IdentityManagementService_Register_FullMethodName,
+	}
+
+	// Check if the method is in the list of methods to not check
+	for _, method := range methods {
+		if method == info.FullMethod {
+			return handler(ctx, req)
+		}
+	}
+
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Error(codes.InvalidArgument, "Missing metadata")
+	}
+
+	// Check if the token is in the metadata md["X-AUTH-TOKEN"][0]
+	log.Println(md)
+	if len(md["x-auth-token"]) == 0 {
+		return nil, status.Error(codes.Unauthenticated, "Missing token")
+	}
+
+	token := md["x-auth-token"][0]
+	if !user.VerifyAuthToken(ctx, token) {
+		return nil, status.Error(codes.Unauthenticated, "Invalid token")
+	}
+
+	// continue on
+	return handler(ctx, req)
+
+}
