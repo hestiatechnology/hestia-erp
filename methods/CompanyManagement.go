138c6fcfc6abc73aae788326ad0777d633131692
danielalexis <danielalexis.o.pereira@gmail.com>
2024-02-22 14:14:22 +0000

    Add CreateCompany method to CompanyManagementServer

--- methods/CompanyManagement.go
+++ methods/CompanyManagement.go
@@ -1,9 +1,14 @@
 package methods
 
 import (
+	"context"
 	"hestia/api/pb"
 )
 
 type CompanyManagementServer struct {
 	pb.UnimplementedCompanyManagementServer
+}
+
+func (s *CompanyManagementServer) CreateCompany(ctx context.Context, in *pb.CreateCompanyRequest) (*pb.IdResponse, error) {
+	return &pb.IdResponse{}, nil
 }
