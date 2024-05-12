af86db5eef51cc441bda46f654f345b141c64c57
danielalexis <danielalexis.o.pereira@gmail.com>
2024-05-12 19:26:55 +0100

    chore: Update logger package

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"log"
+	"hestia/api/utils/log"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 
 	"hestia/api/interceptor"
@@ -15,15 +16,18 @@ import (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":9000")
+
+	PORT := 9000
+	lis, err := net.Listen("tcp", ":"+strconv.Itoa(PORT))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.ErrorLogger.Fatalf("failed to listen: %v", err)
 	}
+	log.InfoLogger.Println("Server listening on port", PORT)
 	s := grpc.NewServer(grpc.ChainUnaryInterceptor(interceptor.AuthInterceptor))
 
 	if strings.ToLower(os.Getenv("ENV")) == "development" {
-		log.Println("Running in development mode")
-		log.Println("Registering reflection service")
+		log.WarningLogger.Println("Running in development mode")
+		log.WarningLogger.Println("Registering reflection service")
 		reflection.Register(s)
 	}
 
@@ -32,6 +36,6 @@ func main() {
 	pb.RegisterTextileServer(s, &methods.TextileServer{})
 	pb.RegisterCompanyManagementServer(s, &methods.CompanyManagementServer{})
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.ErrorLogger.Fatalf("failed to serve: %v", err)
 	}
 }
--- utils/log/log.go
+++ utils/log/log.go
@@ -1,55 +1,26 @@
 package log
 
 import (
+	"io"
 	"log"
-	"time"
+	"os"
 )
 
-const (
-	// YYYY-MM-DD: 2022-03-23
-	YYYYMMDD = "2006-01-02"
-	// 24h hh:mm:ss: 14:23:20
-	HHMMSS24h = "15:04:05"
-	// 12h hh:mm:ss: 2:23:20 PM
-	HHMMSS12h = "3:04:05 PM"
-	// text date: March 23, 2022
-	TextDate = "January 2, 2006"
-	// text date with weekday: Wednesday, March 23, 2022
-	TextDateWithWeekday = "Monday, January 2, 2006"
-	// abbreviated text date: Mar 23 Wed
-	AbbrTextDate = "Jan 2 Mon"
+var (
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
 )
 
-func Info(msg string) {
-	flags := log.Lshortfile
-	// Log without log level
-	datetime := time.Now().UTC().Format(YYYYMMDD+" "+HHMMSS24h) + ": "
-	log.SetFlags(flags)
-	// INFO log level
-	log.SetPrefix("INFO: " + datetime)
-	log.Println(msg)
-}
+func init() {
+	file, err := os.OpenFile("", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-func Warn(msg string) {
-	flags := log.Lshortfile
-	// Log without log level
-	datetime := time.Now().UTC().Format(YYYYMMDD+" "+HHMMSS24h) + ": "
-	log.SetFlags(flags)
-	// INFO log level
-	log.SetPrefix("WARN: " + datetime)
-	log.Println(msg)
-}
-
-func Error(msg string) {
-	flags := log.Lshortfile
-	// Log without log level
-	datetime := time.Now().UTC().Format(YYYYMMDD+" "+HHMMSS24h) + ": "
-	log.SetFlags(flags)
-	// INFO log level
-	log.SetPrefix("ERROR: " + datetime)
-	log.Println(msg)
-}
-
-func Fatalf(msg string, err ...any) {
-	log.Fatalf(msg, err...)
+	mw := io.MultiWriter(os.Stdout, file)
+	mwError := io.MultiWriter(os.Stderr, file)
+	InfoLogger = log.New(mw, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(mw, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(mwError, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
