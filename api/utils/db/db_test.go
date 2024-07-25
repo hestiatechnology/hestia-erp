e15bcced69aec238e43721700315cbaf7bdd8ea4
danielalexis <danielalexis.o.pereira@gmail.com>
2024-07-26 00:29:48 +0100

    feat(api): Add unit tests for GetDbPoolConn function in db_test.go

---
+++ api/utils/db/db_test.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"testing"
+)
+
+func TestGetDbPoolConn(t *testing.T) {
+	tests := []struct {
+		name       string
+		wantErr    bool
+		beforeFunc func()
+	}{
+		{
+			name:    "Success",
+			wantErr: false,
+		},
+		/*{
+			name:    "Error",
+			wantErr: true,
+			beforeFunc: func() {
+				os.Setenv("PGUSER", "doesnotexist")
+				os.Setenv("PGPASSWORD", "1234567890")
+				os.Setenv("PGDATABASE", "dbthatdoesnt exist")
+				os.Setenv("PGHOST", "localhost")
+			},
+		},*/
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := GetDbPoolConn()
+			if tt.beforeFunc != nil {
+				tt.beforeFunc()
+			}
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetDbPoolConn() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
