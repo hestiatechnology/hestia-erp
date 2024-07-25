f0a5be4038b8839b29fb107ae629b9cbe204d947
danielalexis <danielalexis.o.pereira@gmail.com>
2024-07-26 00:29:55 +0100

    feat(web): Add Textile navbar component and routes

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -3,10 +3,19 @@ import { LoginComponent } from './auth/login/login.component';
 import { TestComponent } from './test/test.component';
 import { ForgotPasswordComponent } from "./auth/forgot-password/forgot-password.component";
 import { SsoComponent } from "./auth/sso/sso.component";
+import { HomeComponent } from "./textile/home/home.component";
+import { NavbarComponent as TextileNavbarComponent } from "./textile/navbar/navbar.component";
 
 export const routes: Routes = [
   { "path": "login", component: LoginComponent },
   { "path": "sso", component: SsoComponent },
   { "path": "forgotpassword", component: ForgotPasswordComponent },
+  {
+    "path": "textile",
+    component: TextileNavbarComponent,
+    children: [
+      { "path": "", component: HomeComponent }
+    ]
+  },
   { "path": "test", component: TestComponent },
 ];
