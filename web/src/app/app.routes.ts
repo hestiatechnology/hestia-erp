31b8e40952169ce5a8e7345513ae44f2a44188f1
danielalexis <danielalexis.o.pereira@gmail.com>
2024-07-29 13:36:52 +0100

    feat(web): add initial invoice component

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -3,20 +3,33 @@ import { LoginComponent } from './auth/login/login.component';
 import { TestComponent } from './test/test.component';
 import { ForgotPasswordComponent } from "./auth/forgot-password/forgot-password.component";
 import { SsoComponent } from "./auth/sso/sso.component";
-import { HomeComponent } from "./textile/home/home.component";
+import { HomeComponent as TextileHomeComponent } from "./textile/home/home.component";
 import { HomeComponent as TechPackHomeComponent } from "./textile/techpack/home/home.component";
 import { ViewComponent as TechPackViewComponent } from "./textile/techpack/view/view.component";
 import { TextileComponent } from "./textile/textile.component";
+import { InvoiceComponent } from "./invoice/invoice.component";
+import { HomeComponent as InvoiceHomeComponent } from "./invoice/home/home.component";
 
 export const routes: Routes = [
   { "path": "login", component: LoginComponent },
   { "path": "sso", component: SsoComponent },
   { "path": "forgotpassword", component: ForgotPasswordComponent },
+
+  // /invoice
+  {
+    path: "invoice",
+    component: InvoiceComponent,
+    children: [
+      { "path": "", component: InvoiceHomeComponent },
+    ]
+  },
+
+  // /textile
   {
     "path": "textile",
     component: TextileComponent,
     children: [
-      { "path": "", component: HomeComponent },
+      { "path": "", component: TextileHomeComponent },
       { "path": "techpack", component: TechPackHomeComponent },
       { "path": "techpack/new", component: TechPackViewComponent }
     ]
---
+++ web/src/app/invoice/invoice.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+
+@Component({
+  selector: 'app-invoice',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './invoice.component.html',
+  styleUrl: './invoice.component.scss'
+})
+export class InvoiceComponent {
+
+}
