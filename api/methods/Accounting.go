e622f44b93942ec35420d2d0b9fffe5aff6b1caa
Daniel Pereira <danielalexis.o.pereira@gmail.com>
2024-10-08 18:06:34 +0200

    Change database schema for proper invoicing + GetVatRates and GetVatExemptions (#97)
    * change client constraints for proper naming and add document series tables
    
    * feat(db): update Hestia.dbm to add series and number columns in document table, and establish foreign key constraints for series_type_code
    
    * feat(db): enhance Hestia.dbm with new accounting schema, add VAT rate and exemption tables, and establish foreign key relationships for document and series
    
    * feat(db): update Hestia.dbm to enhance VAT exemption and document line tables, add new columns, and establish foreign key constraints
    
    * feat(db): update Hestia.dbm to modify document_line table structure, add new columns for quantity, price, and total, and implement a constraint for VAT rate and exemption
    
    * feat(db): update Hestia.dbm to add vat_type table, include vat_type_id column in vat_rate table, and establish foreign key relationship between vat_rate and vat_type
    
    * feat(api): update Hestia.dbm to enhance document and document_line tables, add new columns for net_total and gross_total, and establish foreign key constraints for VAT rate and exemption
    
    * update protos, and protoc version
    
    * feat(api): add GetVatRates methods
    
    * feat(api): add GetVatExemptions method to retrieve active VAT exemptions
    
    * feat(api): update VAT queries to filter by start and end dates for active rates and exemptions

---
+++ api/methods/Accounting.go
@@ -0,0 +1,81 @@
+package methods
+
+import (
+	"context"
+	"hestia/api/pb/accounting"
+	"hestia/api/utils/db"
+	"hestia/api/utils/logger"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type TaxServer struct {
+	accounting.UnimplementedTaxServer
+}
+
+func (s *TaxServer) GetVatRates(ctx context.Context, _ *emptypb.Empty) (*accounting.VatRates, error) {
+	db, err := db.GetDBPoolConn()
+	if err != nil {
+		logger.ErrorLogger.Println("Unable to connect to db: ", err)
+		return nil, status.Error(codes.Internal, "Unable to connect to db")
+	}
+
+	rows, err := db.Query(ctx, "SELECT r.id, r.rate, t.code, r.country FROM accounting.vat_rate r LEFT JOIN accounting.vat_type t ON r.vat_type_id = t.id WHERE (r.start_date < NOW() AND (r.end_date IS NULL OR r.end_date > NOW()))")
+	if err != nil {
+		logger.ErrorLogger.Println("Unable to query db: ", err)
+		return nil, status.Error(codes.Internal, "Something went wrong")
+	}
+
+	defer rows.Close()
+
+	var vatRates accounting.VatRates
+	for rows.Next() {
+		var vatRate accounting.VatRate
+		// convert uuid to string
+		var id uuid.UUID
+		err = rows.Scan(&id, &vatRate.Rate, &vatRate.Code, &vatRate.Country)
+		if err != nil {
+			logger.ErrorLogger.Println("Unable to scan row: ", err)
+			return nil, status.Error(codes.Internal, "Something went wrong")
+		}
+		vatRate.Id = id.String()
+		vatRates.VatRates = append(vatRates.VatRates, &vatRate)
+	}
+
+	return &vatRates, nil
+}
+
+func (s *TaxServer) GetVatExemptions(ctx context.Context, _ *emptypb.Empty) (*accounting.VatExemptions, error) {
+	db, err := db.GetDBPoolConn()
+	if err != nil {
+		logger.ErrorLogger.Println("Unable to connect to db: ", err)
+		return nil, status.Error(codes.Internal, "Unable to connect to db")
+	}
+
+	rows, err := db.Query(ctx, "SELECT id, code, description FROM accounting.vat_exemption WHERE (start_date < NOW() AND (end_date IS NULL OR end_date > NOW()))")
+	if err != nil {
+		logger.ErrorLogger.Println("Unable to query db: ", err)
+		return nil, status.Error(codes.Internal, "Something went wrong")
+	}
+
+	defer rows.Close()
+
+	var vatExemptions accounting.VatExemptions
+	for rows.Next() {
+		var vatExemption accounting.VatExemption
+		// convert uuid to string
+		var id uuid.UUID
+		err = rows.Scan(&id, &vatExemption.Code, &vatExemption.Description)
+		if err != nil {
+			logger.ErrorLogger.Println("Unable to scan row: ", err)
+			return nil, status.Error(codes.Internal, "Something went wrong")
+		}
+		vatExemption.Id = id.String()
+		vatExemptions.VatExemptions = append(vatExemptions.VatExemptions, &vatExemption)
+	}
+
+	return &vatExemptions, nil
+}
