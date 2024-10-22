e3d1912bd6c29f02da16e2e72bcd9cdd11c894a6
danielalexis <danielalexis.o.pereira@gmail.com>
2024-10-22 17:20:06 +0100

    feat(api): add SSE-B2 default encryption and upload file functionality

--- api/utils/storage/storage.go
+++ api/utils/storage/storage.go
@@ -20,7 +20,7 @@ func CreateS3Bucket(companyId uuid.UUID) error {
 	}
 
 	sess, err := session.NewSession(&aws.Config{
-		Credentials: credentials.NewStaticCredentials("003dfe653b1e4290000000001", "K003if50xGYcjc6SnsPqKoNYs2HE+I8", ""),
+		Credentials: credentials.NewStaticCredentials("--------", "-------------", ""),
 		// Backblaze endpoint
 		Endpoint: aws.String("https://s3.eu-central-003.backblazeb2.com"),
 		Region:   aws.String("eu-central-003")},
@@ -81,5 +81,60 @@ func CreateS3Bucket(companyId uuid.UUID) error {
 	}
 
 	log.Info().Str("name", bucketName).Msg("CORS rules applied")
+
+	// Enable SSE-B2 default encryption
+	_, err = svc.PutBucketEncryption(&s3.PutBucketEncryptionInput{
+		Bucket: aws.String(bucketName),
+		ServerSideEncryptionConfiguration: &s3.ServerSideEncryptionConfiguration{
+			Rules: []*s3.ServerSideEncryptionRule{
+				{
+					ApplyServerSideEncryptionByDefault: &s3.ServerSideEncryptionByDefault{
+						SSEAlgorithm: aws.String("AES256"),
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
+		log.Error().Err(err).Str("name", bucketName).Msg("failed to enable SSE-B2 default encryption")
+		return fmt.Errorf("failed to enable SSE-B2 default encryption: %v", err)
+	}
+
+	log.Info().Str("name", bucketName).Msg("SSE-B2 default encryption enabled")
 	return nil
+}
+
+// Returns a pre-signed URL for the given object key
+// Filename can be a path
+func UploadFile(companyId uuid.UUID, filename string) (string, error) {
+	bucketName := "hst-" + companyId.String()
+	if strings.ToLower(os.Getenv("ENV")) == "dev" {
+		bucketName = "hst-dev-" + bucketName
+	}
+
+	sess, err := session.NewSession(&aws.Config{
+		Credentials: credentials.NewStaticCredentials("------", "--------------------", ""),
+		// Backblaze endpoint
+		Endpoint: aws.String("https://s3.eu-central-003.backblazeb2.com"),
+		Region:   aws.String("eu-central-003")},
+	)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to create session")
+		return "", fmt.Errorf("failed to create session: %v", err)
+	}
+
+	svc := s3.New(sess)
+
+	req, _ := svc.PutObjectRequest(&s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(filename),
+	})
+
+	url, err := req.Presign(15 * 60)
+	if err != nil {
+		log.Error().Err(err).Str("key", filename).Msg("failed to presign URL")
+		return "", fmt.Errorf("failed to presign URL: %v", err)
+	}
+
+	return url, nil
 }
